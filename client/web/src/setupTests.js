4f4515f568e7819bcbe387ee617d3cc17ffc3f57
germmand <german.aguilera17@gmail.com>
2019-12-07 09:28:16 -0400

    feat: Add script to adjust environment at "runtime"

--- client/web/src/setupTests.js
+++ client/web/src/setupTests.js
@@ -5,3 +5,9 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({
     adapter: new Adapter()
 });
+
+// For future googlers: global *is* window in jest.
+// Here we'll mock the "runtime" environment.
+global._env_ =  jest.fn().mockImplementation(() => ({
+    REACT_APP_DIRMOD_API_DOMAIN: 'http://test.app.com',
+}));
--- client/web/src/utils/http/index.js
+++ client/web/src/utils/http/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_DIRMOD_API_DOMAIN,
+  // We use the window object because this url has to be injected at "runtime" to test multiple environments.
+  // The way that this works is that every time the app is ran (i.e. in a container) a script will read the environment
+  // and adjust the js file that's in charge of changing these values.
+  // This way we can test multiple environments without having to trigger a whole rebuild.
+  baseURL: window._env_.REACT_APP_DIRMOD_API_DOMAIN,
 });
